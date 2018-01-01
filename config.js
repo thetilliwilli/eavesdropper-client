87e0126b6f78092b914aa5fc9dd6b78dca353703
TilliWilli_Home <tilliwilli@gmail.com>
2018-01-01 23:13:31 +0300

    подключил izida-common удалил лишний код

--- config.js
+++ config.js
@@ -3,7 +3,9 @@
 const path = require("path");
 
 module.exports = {
-    schedule: "1 0 * * *",
+    retriveSchedule: "1 0 * * *",
+    syncLastCommitSchedule: "*/5 * * * * *",
+    storagePath: process.env["STORAGE_PATH"] || path.join(__dirname, "Storage"),
     server: {
         ip: process.env["SERVER_IP"] || "212.12.28.107",
         port : process.env["SERVER_PORT"] || 10001,
--- index.js
+++ index.js
@@ -2,20 +2,15 @@
 
 const config = require("./config.js");
 
-const WebServer = require("./WebServer/index.js");
-const Backuper = require("./Backuper/index.js");
+const Retriver = require("./Retriver/index.js");
 
-const webConfig = Object.assign({}, config.webServer, config.fsProxy);
-const backupConfig = Object.assign({}, config.fsProxy, config.backuper);
-
-const webServer = new WebServer(webConfig);
-const backuper = new Backuper(backupConfig);
+const retriver = new Retriver(
+    Object.assign({}, config, config.server)
+);
 
 Promise.resolve()
-    .then(() => webServer.Initialize())
-    .then(() => backuper.Initialize())
-    .then(() => backuper.StartServer())
-    .then(() => webServer.StartServer())
+    .then(() => retriver.Initialize())
+    .then(() => retriver.StartServer())
     .then(() => {
         console.log(`Инициализация всех сервисов прошла успешно`);// do what u want here
     })
